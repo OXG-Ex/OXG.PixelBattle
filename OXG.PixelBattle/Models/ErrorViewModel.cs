8d527b48e2b652420297ca3552a3a7b374c057dc
Назаренко Семён <semes212@gmail.com>
2020-05-21 14:48:52 +0300

    Добавьте файлы проекта.

---
+++ OXG.PixelBattle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OXG.PixelBattle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
